caee8b815c12791d5bcff05a23bbdedc29ee8ec0
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-07-26 21:37:03 +0530

    for loop 2

---
+++ lecture4_2.java
@@ -0,0 +1,8 @@
+
+public class lecture4_2 {
+    public static void main(String[] args) {
+        for(int counter=0;counter<=10;counter++){
+            System.out.print(counter+" ");
+        }
+    }
+}
