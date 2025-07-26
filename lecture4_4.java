075fb8fef81f08007bced27fc9597a1fba77eb90
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-07-26 21:44:38 +0530

    do while loop

---
+++ lecture4_4.java
@@ -0,0 +1,9 @@
+public class lecture4_4 {
+    public static void main(String[] args) {
+        int i=0;
+        do{
+            System.out.print(i+" ");
+            i++;
+        }while(i<11);
+    }
+}
