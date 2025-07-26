3d42b05939b5f79cb23a3efc06ad425b1926a047
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-07-26 21:39:34 +0530

    While Loop

---
+++ lecture4_3.java
@@ -0,0 +1,9 @@
+public class lecture4_3 {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<=10){
+            System.out.print(i+" ");
+            i=i+1;
+        }
+    }
+}
