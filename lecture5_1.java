cd046732fb9848d31313ca123b2070b45ee07279
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-07-28 21:24:36 +0530

    Square Patter in Java

---
+++ lecture5_1.java
@@ -0,0 +1,11 @@
+//Solid rectangle pattern
+public class lecture5_1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print(" * ");
+            }
+            System.out.println("");
+        }
+    }
+}
