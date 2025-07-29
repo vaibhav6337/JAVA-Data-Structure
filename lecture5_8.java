8758ab6b94c2690361fb0073ae761ed29a10e1b2
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-07-29 19:34:32 +0530

    inverted Half Pyramid with numbers

---
+++ lecture5_8.java
@@ -0,0 +1,12 @@
+ // inverted Half Pyramid with numbers
+public class lecture5_8 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
