44f77e5181a344533a9c75d939e1e814c47e0620
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-07-29 19:22:12 +0530

    Half Pyramid with numbers

---
+++ lecture5_6.java
@@ -0,0 +1,13 @@
+//Half Pyramid with numbers
+public class lecture5_6 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=5;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
