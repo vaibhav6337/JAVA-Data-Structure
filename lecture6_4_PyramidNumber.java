83df7ecf19ed15945607c8e7a7cc8db8aa493af7
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-08-01 21:19:55 +0530

    Pyramid Number Pattern

---
+++ lecture6_4_PyramidNumber.java
@@ -0,0 +1,16 @@
+//Pyramid Number Pattern
+
+public class lecture6_4_PyramidNumber {
+public static void main(String[] args) {
+    int n=8;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+}    
+}
