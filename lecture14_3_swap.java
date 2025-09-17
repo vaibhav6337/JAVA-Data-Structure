29d21b788dc2c3d80c677437cd76e962e709db25
vaibhav bhagat <140892436+vaibhav6337@users.noreply.github.com>
2025-09-17 13:32:36 +0530

    swapping of numbers(call by value)

---
+++ lecture14_3_swap.java
@@ -0,0 +1,15 @@
+public class lecture14_3_swap {
+    public static void swap(int a, int b){
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After Swapping");
+        System.out.println("a = "+ a);
+        System.out.println("b = "+ b);
+    }
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        swap(a,b);
+    }
+}
