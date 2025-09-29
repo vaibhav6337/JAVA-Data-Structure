6844615631695832d53a9e17b6f8915b513e7ee9
vaibhav bhagat <140892436+vaibhav6337@users.noreply.github.com>
2025-09-29 12:24:27 +0530

    Add files via upload

---
+++ lecture16_1_array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class lecture16_1_array {
+    public static void main(String[] args) {
+        int mark[]=new int[50];
+        Scanner sc=new Scanner(System.in);
+        mark[0]=sc.nextInt();
+        mark[1]=sc.nextInt();
+        mark[2]=sc.nextInt();
+        mark[3]=sc.nextInt();
+
+        System.out.println(mark[0]);
+        System.out.println(mark[1]);
+        System.out.println(mark[2]);
+        System.out.println(mark[3]);
+
+    }
+}
