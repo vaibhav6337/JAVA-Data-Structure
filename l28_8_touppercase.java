5a7beff6be7ee557f7c702a95cd198aa0f019146
vaibhav bhagat <140892436+vaibhav6337@users.noreply.github.com>
2026-01-25 17:34:37 +0530

    convert into upper case

---
+++ l28_8_touppercase.java
@@ -0,0 +1,23 @@
+public class l28_8_touppercase {
+    public static String toUpperCase(String str){
+        StringBuilder sb=new StringBuilder("");
+        sb.append(Character.toUpperCase(str.charAt(0)));
+
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }else{
+                sb.append(str.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str="hi how are You ";
+        System.out.println(toUpperCase(str));
+    }
+}
