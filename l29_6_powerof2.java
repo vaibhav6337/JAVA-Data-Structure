106935cd40692efe6bb64c32d8ed806ee4c0357d
vaibhav bhagat <140892436+vaibhav6337@users.noreply.github.com>
2026-02-26 20:35:42 +0530

    bit manipulation

---
+++ l29_6_powerof2.java
@@ -0,0 +1,8 @@
+public class l29_6_powerof2 {
+    public static boolean isPowerOfTwo(int n){
+        return (n&(n-1))==0;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(9));
+    }
+}
